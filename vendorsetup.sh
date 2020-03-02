PixelExperience-Devices/device_xiaomi_sirius

sirius: reworked tree for builds with vendor

vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo aosp_sirius-userdebug
+add_lunch_combo lineage_sirius-userdebug
 

fuldaros/device_xiaomi_grus-wip

vendorsetup.sh
@@ -1,0 +1,2 @@
+add_lunch_combo lineage_grus-userdebug
+